57b84892c2167320d44c781f54b3b83acc0ce880
erfan <erfano2013@gmail.com>
2025-11-05 17:16:53 +0330

    build atentication ui (lofin page/ sign up page)

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-veiw";
+
+const page = () => {
+  return (
+    <div>
+      <SignUpView />
+      </div>
+  ) 
+}   
+export default page;
